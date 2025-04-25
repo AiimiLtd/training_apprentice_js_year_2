8c64ce0c0d3892b3eddb31a60b2e670c01909ee4
Justin Heath <just.coder@outlook.com>
2025-04-25 15:50:31 +0100

    example

--- app.js
+++ app.js
@@ -1,1 +1,82 @@
-console.log('Hello Aiimi\'ers');
+const mainUrl = 'https://jsonplaceholder.typicode.com/photos';
+
+async function getData(url) {
+  try {
+    let response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return await response.json();
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+}
+
+
+// getData(mainUrl).then(data => {
+//   if (data) {
+//     console.log('Fetched Data:', data);
+//   }
+// });
+
+function createCard(item) {
+  // Create card container div
+  let cardDiv = document.createElement('div');
+  cardDiv.className = 'col-md-4';
+
+  // Create card
+  let card = document.createElement('div');
+  card.className = 'card mb-4';
+
+  // Create card image
+  let cardImg = document.createElement('img');
+  cardImg.className = 'card-img-top';
+  cardImg.src = item.url;
+  cardImg.alt = item.title;
+
+  cardImg.onerror = function () {
+    this.src = 'https://via.placeholder.com/600/92c952';
+  };
+
+  // Create card body
+  let cardBody = document.createElement('div');
+  cardBody.className = 'card-body';
+
+  // Create card title
+  let cardTitle = document.createElement('h5');
+  cardTitle.className = 'card-title';
+  cardTitle.textContent = item.title;
+
+  // Create card text
+  let cardText = document.createElement('p');
+  cardText.className = 'card-text';
+  cardText.textContent = item.id;
+
+  // Append title and text to card body
+  cardBody.appendChild(cardTitle);
+  cardBody.appendChild(cardText);
+
+  // Append image and body to card
+  card.appendChild(cardImg);
+  card.appendChild(cardBody);
+
+  // Append card to card container div
+  cardDiv.appendChild(card);
+
+  return cardDiv;
+}
+
+// Function to inject cards into the DOM
+async function injectCards(url) {
+  let data = await getData(url);
+  if (data && Array.isArray(data)) {
+    let cardContainer = document.getElementById('card-container');
+    data.forEach(item => {
+      let cardElement = createCard(item);
+      cardContainer.appendChild(cardElement);
+    });
+  }
+}
+
+injectCards(mainUrl)
